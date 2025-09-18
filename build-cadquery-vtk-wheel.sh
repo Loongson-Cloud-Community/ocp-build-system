31b2458d702c9160b429ebb64ce45774ab4b7a04
wangweijie <wangweijie@loongson.cn>
2025-09-18 16:43:15 +0800

    feat: add build 9.3.1 cp313 cadquery_vtk wheel script

---
+++ build-cadquery-vtk-wheel.sh
@@ -0,0 +1,75 @@
+#/bin/bash
+set -eu
+
+PYTHONUTF8=1
+VTK=${VTK:-9.3.1}
+VTK_MAJOR=${VTK_MAJOR:-9.3}
+
+PYTHON_BIN=${PYTHON_BIN:-python3.13}
+VENV_DIR=${VENV_DIR:-.build-vtk}
+
+# Dowload ocp-build-system sources
+git clone https://github.com/CadQuery/ocp-build-system.git
+
+cd ocp-build-system
+
+cpu_count=$(nproc)
+
+echo "=> Using $cpu_count CPUs"
+
+# Create python3.13 venv
+if ! command -v "$PYTHON_BIN" >/dev/null 2>&1; then
+    echo "Error: $PYTHON_BIN not found in PATH" >&2
+    exit 1
+fi
+
+if [ -d "$VENV_DIR" ]; then
+    echo "Removing existing venv at $VENV_DIR"
+    rm -rf "$VENV_DIR"
+fi
+
+$PYTHON_BIN -m venv $VENV_DIR
+source $VENV_DIR/bin/activate
+
+pip install --upgrade --no-cache-dir pip wheel build setuptools
+
+# Debian Deps
+apt install -y build-essential cmake mesa-common-dev mesa-utils freeglut3-dev python3-dev python3-venv git-core ninja-build wget libglvnd0 libglvnd-dev curl
+
+# Download VTK sources
+curl -L -O https://vtk.org/files/release/$VTK_MAJOR/VTK-$VTK.tar.gz
+tar -zxf VTK-$VTK.tar.gz
+
+cd VTK-$VTK
+mkdir build
+
+patch -p1 < ../patches/vtk-$VTK/9987_try_except_python_import.patch
+patch -p1 < ../patches/vtk-$VTK/11486.patch
+sed -i '/defined(__riscv)/a\    defined(__loongarch64) || \\' ThirdParty/doubleconversion/vtkdoubleconversion/double-conversion/utils.h
+sed -i 's/_M_chilren/m_children/' Utilities/octree/octree/octree_node.txx
+
+# Build Linux python libraries from Scratch
+export CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" # Disables the C++11 ABI features for VTK compatibility
+
+cd build
+cmake -G Ninja \
+      -D VTK_VERSIONED_INSTALL=ON \
+      -D VTK_CUSTOM_LIBRARY_SUFFIX="9.3" -DVTK_VERSION_SUFFIX="" \
+      -D VTK_WHEEL_BUILD=ON -DVTK_WRAP_PYTHON=ON \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_POLICY_DEFAULT_CMP0174=NEW -DCMAKE_POLICY_DEFAULT_CMP0177=NEW \
+      ..
+
+ninja -j $cpu_count
+
+# Build wheel
+sed -i "s/dist_name = 'vtk'/dist_name = 'cadquery_vtk'/" setup.py
+sed -i "s/version_suffix = '9.3'/version_suffix = ''/" setup.py
+cat setup.py
+$PYTHON_BIN -m build -n -w
+
+# repair wheel
+pip install auditwheel patchelf
+auditwheel repair --wheel-dir wheel dist/*.whl
+rm dist/*.whl
+mv wheel/*.whl dist/
